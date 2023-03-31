964307602efad1a9d7b26661a726ec7edbf434b4
devbotas <simon@devbot.lt>
2023-03-31 11:35:41 +0300

    Updating.

---
+++ Tech.Tevux.Dashboards.Controls/Code/MyLibrary.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Tech.Tevux.Dashboards.Controls;
+
+public class MyLibrary : IDashboardControlProvider {
+    public List<System.Type> GetDashboardControls() {
+        return new List<System.Type> { typeof(Label) };
+    }
+}
