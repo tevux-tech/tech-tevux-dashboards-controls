62529dc12b7d2279a76ccdd3a02dfc4d94e56b96
devbotas <simon@devbot.lt>
2023-03-31 12:10:22 +0300

    Releasing 0.24.0-preview.2.

--- Tech.Tevux.Dashboards.Controls/Code/MyLibrary.cs
+++ Tech.Tevux.Dashboards.Controls/Code/MyLibrary.cs
@@ -3,6 +3,8 @@
 namespace Tech.Tevux.Dashboards.Controls;
 
 public class MyLibrary : IDashboardControlProvider {
+    public static MyLibrary Instance { get; } = new MyLibrary();
+
     public List<System.Type> GetDashboardControls() {
         return new List<System.Type> { typeof(Label) };
     }
