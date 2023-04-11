b0ee986ef2fd5992e1a76f27bc6de0477876ef88
devbotas <simon@devbot.lt>
2023-04-11 17:12:12 +0300

    Accidentally excluded the only control this lib provides.

--- Tech.Tevux.Dashboards.Controls/Code/MyLibrary.cs
+++ Tech.Tevux.Dashboards.Controls/Code/MyLibrary.cs
@@ -7,6 +7,10 @@ public class MyLibrary : ILibrary, IDashboardControlProvider {
     public static MyLibrary Instance { get; } = new MyLibrary();
     public List<System.Type> DashboardControls { get; private set; } = new();
 
+    private MyLibrary() {
+        DashboardControls.Add(typeof(Label));
+    }
+
     public void Dispose() {
         // A good article explaining how to implement Dispose. https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose
         Dispose(true);
